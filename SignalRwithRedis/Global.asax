786fd299b4db02ae25526b2155da16cd2ebeadf0
jun78 <jun_78@hotmail.com>
2014-02-17 13:22:38 +1000

    Initial check-in of solution

---
+++ SignalRwithRedis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRwithRedis.MvcApplication" Language="C#" %>
---
+++ SignalRwithRedis/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR;
+[assembly: OwinStartup(typeof(SignalRwithRedis.Startup))]
+
+namespace SignalRwithRedis
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			// Any connection or hub wire up and configuration should go here
+      //Use the line below to use signalr only.  
+			//app.MapSignalR();
+
+			GlobalHost.DependencyResolver.UseRedis("localhost", 6379, string.Empty, "myApp");
+			app.MapHubs();
+		}
+	}
+}
